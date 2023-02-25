12c8e7cd9a8eeb7539e72c5927131f4686832329
abdallah96 <gueye.amadou1996@gmail.com>
2023-02-25 17:37:26 +0100

    first-commit

---
+++ D-Braille/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  D-Braille
+//
+//  Created by Abdallah Amadou Gueye on 25.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0, green: 0.102, blue: 0.545).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 57) {
+                Text("BRAILLE LERNEN")
+                    .font(.largeTitle).bold()
+                    .foregroundColor(Color(red: 1, green: 0.929, blue: 0.063))
+                HStack( spacing: 86) {
+                    Image("Buchstaben lernen").accessibilityLabel("Buchstaben lernen")
+                    Image("Buchstaben üben").accessibilityLabel("Buchstaben üben")
+                }
+                HStack( spacing: 86) {
+                    Image("Zahlen lernen").accessibilityLabel("Zahlen lernen")
+                    Image("Zahlen üben").accessibilityLabel("Zahlen üben")
+                }
+                HStack( spacing: 86) {
+                    Image("Wörter lernen").accessibilityLabel("Wörter lernen")
+                    Image("Wörter üben").accessibilityLabel("Wörter üben")
+                }
+                Image("ANFANG")
+            }
+            
+            VStack {
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ D-Braille/D_BrailleApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_BrailleApp.swift
+//  D-Braille
+//
+//  Created by Abdallah Amadou Gueye on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct D_BrailleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
